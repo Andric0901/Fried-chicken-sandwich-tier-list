eca72d1448e29c0f6b5df0b39361e34b7e8d1832
Andric0901 <75761073+Andric0901@users.noreply.github.com>
2024-09-03 18:42:13 +0000

    Add unit test

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+from PIL import Image
+
+
+LOGOS_PATH = "logos"
+
+
+class TestLogosRatio(unittest.TestCase):
+    def test_logo_ratio_one_to_one(self):
+        list_of_logo_names = os.listdir(LOGOS_PATH)
+        for name in list_of_logo_names:
+            logo_dir = LOGOS_PATH + "/" + name
+            image = Image.open(logo_dir)
+            width, height = image.size
+            try:
+                assert width == height
+                image.close()
+            except AssertionError:
+                print(width, height, logo_dir)
+
+
+if __name__ == '__main__':
+    unittest.main()
