0bbb1dc086c9b7dc4cb514e35454afa7796c59e1
Andric0901 <75761073+Andric0901@users.noreply.github.com>
2024-09-08 22:44:38 +0000

    New unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,18 @@
 import unittest
 import os
+import json
 from PIL import Image
 
 
 LOGOS_PATH = "logos"
 
 
-class TestLogosRatio(unittest.TestCase):
-    def test_logo_ratio_one_to_one(self):
-        list_of_logo_names = os.listdir(LOGOS_PATH)
-        for name in list_of_logo_names:
+class TestLogos(unittest.TestCase):
+    def setUp(self):
+        self.list_of_logo_names = os.listdir(LOGOS_PATH)
+
+    def test_logo_ratio_is_one_to_one(self):
+        for name in self.list_of_logo_names:
             logo_dir = LOGOS_PATH + "/" + name
             image = Image.open(logo_dir)
             width, height = image.size
@@ -19,15 +22,36 @@ class TestLogosRatio(unittest.TestCase):
             except AssertionError:
                 print(width, height, logo_dir)
 
-
-class TestLogosExtension(unittest.TestCase):
     def test_logo_extension_equals_jpg(self):
-        list_of_logo_names = os.listdir(LOGOS_PATH)
-        for name in list_of_logo_names:
+        for name in self.list_of_logo_names:
             try:
                 assert name.endswith(".jpg")
             except AssertionError:
                 print(name)
+
+
+class TestTierDictJson(unittest.TestCase):
+    def setUp(self):
+        self.f = open('tier_dict.json')
+        self.d = dict(json.load(self.f))
+        self.tiers = list(self.d.keys())
+
+    def test_tier_dict_json_has_unique_names(self):
+        try:
+            for t in self.tiers:
+                # Set removes duplicate elements if there exists one
+                restaurant_names_list, restaurant_names_set = list(self.d[t].keys()), set(self.d[t].keys())
+                assert len(restaurant_names_list) == len(restaurant_names_set)
+        except AssertionError:
+            print(restaurant_names_list, restaurant_names_set)
+            print(len(restaurant_names_list), len(restaurant_names_set))
+
+    # def test_tier_dict_name_matches_logo_filename(self):
+    #     ...
+
+    def tearDown(self):
+        self.f.close()
+
 
 
 if __name__ == '__main__':
