6cc61c165b222f32cb361f9eb959d5a5fa6be3d5
Andric0901 <75761073+Andric0901@users.noreply.github.com>
2024-09-14 18:18:29 +0000

    Add debug code

--- tierlist.py
+++ tierlist.py
@@ -1,5 +1,6 @@
 """A Python file with Tierlist configurations."""
 import os
+import time
 
 from PIL import Image, ImageDraw, ImageFont
 from helper import TIER_DICT, TIER_COLOUR_HEX_DICT, TIERLIST_IMAGE_NAME, TIERLIST_IMAGE_NAME_WITH_YEAR_TAG, TIERLIST_IMAGE_NAME_WITH_YEAR_FIRST_VISITED_TAG
@@ -13,10 +14,14 @@ GAPS_BETWEEN_RESTAURANTS = 10
 TAG_LARGE_SIZE_MULTIPLIER = 5  # Tag's height will be 1/5 of the logo's height
 TAG_SMALL_SIZE_MULTIPLIER = 7  # Tag's height will be 1/7 of the logo's height
 
+DEBUG = False
+
 
 def evaluate_num_logos_per_row(min_val: int = 10, threshold: int = 10) -> int:
     """Evaluate the ideal number of logos per row, such that
     the ratio between the width and height is closest to the golden ratio."""
+    if DEBUG:
+        start = time.time()
     min_difference = 100
     num_logos_per_row = 0
     for i in range(min_val, min_val + threshold):
@@ -30,17 +35,25 @@ def evaluate_num_logos_per_row(min_val: int = 10, threshold: int = 10) -> int:
         if current_diff < min_difference:
             min_difference = current_diff
             num_logos_per_row = i
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for evaluate_num_logos_per_row({min_val}, {threshold}): {end - start} seconds")
     return num_logos_per_row
 
 
 def get_num_rows_per_tier(num_rows: int) -> dict:
     """Get the number of rows per tier, given the number of logos per row."""
+    if DEBUG:
+        start = time.time()
     tier_num_rows = {}
     for k in TIER_DICT:
         if len(TIER_DICT[k]) % num_rows == 0:
             tier_num_rows[k] = len(TIER_DICT[k]) // num_rows
         else:
             tier_num_rows[k] = len(TIER_DICT[k]) // num_rows + 1
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for get_num_rows_per_tier({num_rows}): {end - start} seconds")
     return tier_num_rows
 
 
@@ -50,12 +63,10 @@ BACKGROUND_COLOUR = (26, 26, 26)
 TIER_NUM_ROWS = get_num_rows_per_tier(NUM_LOGOS_PER_ROW)
 
 
-def total_num_restaurants():
-    return sum(TIER_NUM_ROWS[tier] for tier in TIER_NUM_ROWS)
-
-
 def make_tier_indicator(tier):
     """A tier indicator is a square of 100 x 100 pixels, with the text centered"""
+    if DEBUG:
+        start = time.time()
     color = TIER_COLOUR_HEX_DICT[tier]
     image_width, image_height = DEFAULT_WIDTH, \
         DEFAULT_WIDTH * TIER_NUM_ROWS[tier] + GAPS_BETWEEN_RESTAURANTS * (TIER_NUM_ROWS[tier] - 1)
@@ -68,15 +79,23 @@ def make_tier_indicator(tier):
     left, top, right, bottom = draw.textbbox((0, 0), tier, font=font)
     w, h = right - left, bottom - top
     draw.text(((image_width - w) / 2, (image_height - h) / 2 - 4), tier, font=font, fill=(0, 0, 0))
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for make_tier_indicator({tier}): {end - start} seconds")
     return img
 
 
 def make_tier_background(tier):
     """A tier background is a rectangle which can be extended to the right
     color: (26, 26, 26))"""
+    if DEBUG:
+        start = time.time()
     background_width, background_height = BACKGROUND_WIDTH, \
         DEFAULT_WIDTH * TIER_NUM_ROWS[tier] + GAPS_BETWEEN_RESTAURANTS * (TIER_NUM_ROWS[tier] - 1)
     img = Image.new('RGB', (background_width, background_height), BACKGROUND_COLOUR)
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for make_tier_background({tier}): {end - start} seconds")
     return img
 
 
@@ -85,12 +104,18 @@ def _resize_tag_image(filepath: str, small: bool = False):
     
     By default, it will resize it to a larger sized tag, unless specified by the `small` parameter.
     """
+    if DEBUG:
+        start = time.time()
     img = Image.open(filepath)
     width, height = img.size
     size_multiplier = TAG_LARGE_SIZE_MULTIPLIER
     if small:
         size_multiplier = TAG_SMALL_SIZE_MULTIPLIER
-    return img.resize((int(width * DEFAULT_WIDTH / (height * size_multiplier)), int(DEFAULT_WIDTH / size_multiplier)))
+    img = img.resize((int(width * DEFAULT_WIDTH / (height * size_multiplier)), int(DEFAULT_WIDTH / size_multiplier)))
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for _resize_tag_image({filepath}, {small}): {end - start} seconds")
+    return img
 
 TAGS_BASE_PATH = "assets/png/"
 def _make_tags_image_dict():
@@ -100,6 +125,8 @@ def _make_tags_image_dict():
         - Year tags denoted by 2022-...png (small size by default for alternative tierlist image)
         - Vegan tag denoted by Vegan.png ("vegan_small" if year tags present, "vegan_large" otherwise)
     """
+    if DEBUG:
+        start = time.time()
     tags_image_dict = {}
     png_names = os.listdir(TAGS_BASE_PATH)
     for name in png_names:
@@ -114,13 +141,17 @@ def _make_tags_image_dict():
             else:
                 # Year tag
                 tags_image_dict[int(head)] = _resize_tag_image(filepath, small=True)
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for _make_tags_image_dict(): {end - start} seconds")
     return tags_image_dict
 TAGS_IMAGE_DICT = _make_tags_image_dict()
 
 
 def make_tier_restaurants(tier, with_year_tag: bool = False, with_year_first_visited_tag: bool = False):
     """Make a tier image"""
-    # TODO: some variables here do not need to be resized every iteration, pull out as constants
+    if DEBUG:
+        start = time.time()
     tier_img = make_tier_background(tier)
     restaurants = []
     for restaurant_name in TIER_DICT[tier]:
@@ -138,7 +169,7 @@ def make_tier_restaurants(tier, with_year_tag: bool = False, with_year_first_vis
         # get the width and height of the restaurant's logo
         width, height = logo_img.size
         # resize the logo, preserving the aspect ratio, so that the height is 100 pixels
-        # TODO: store the resized logo image in a db
+        # TODO: resizing is expensive over hundreds of iterations, maybe store in db?
         logo_img = logo_img.resize((int(width * DEFAULT_WIDTH / height), DEFAULT_WIDTH))
         logo_img.paste(price_img, (logo_img.size[0] - price_img.size[0], 0), price_img)
         if is_vegan:
@@ -166,11 +197,16 @@ def make_tier_restaurants(tier, with_year_tag: bool = False, with_year_first_vis
             y_offset += restaurant_height + GAPS_BETWEEN_RESTAURANTS
         tier_img.paste(restaurant_logo, (x_offset, y_offset))
         x_offset += restaurant_logo.size[0] + GAPS_BETWEEN_RESTAURANTS
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for make_tier_restaurants({tier}, {with_year_tag}, {with_year_first_visited_tag}): {end - start} seconds")
     return tier_img
 
 
 def make_one_complete_tier(tier, with_year_tag: bool = False, with_year_first_visited_tag: bool = False):
     """Make a tier image"""
+    if DEBUG:
+        start = time.time()
     tier_indicator = make_tier_indicator(tier)
     tier_restaurants = make_tier_restaurants(tier, with_year_tag=with_year_tag, with_year_first_visited_tag=with_year_first_visited_tag)
     tier_img = Image.new('RGB', (tier_indicator.size[0] + tier_restaurants.size[0] + DEFAULT_GAP,
@@ -178,11 +214,16 @@ def make_one_complete_tier(tier, with_year_tag: bool = False, with_year_first_vi
                                  GAPS_BETWEEN_RESTAURANTS * (TIER_NUM_ROWS[tier] - 1)), (0, 0, 0))
     tier_img.paste(tier_indicator, (0, 0))
     tier_img.paste(tier_restaurants, (tier_indicator.size[0] + DEFAULT_GAP, 0))
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for make_one_complete_tier({tier}, {with_year_tag}, {with_year_first_visited_tag}): {end - start} seconds")
     return tier_img
 
 
 def make_tierlist(with_year_tag: bool = False, with_year_first_visited_tag: bool = False):
     """Make a tierlist image, with margins equal to DEFAULT_GAP"""
+    if DEBUG:
+        start = time.time()
     if with_year_tag and with_year_first_visited_tag:
         # This function should not be called with both booleans set to True
         raise ValueError('make_tierlist should not be called with both year tag booleans set to True')
@@ -196,6 +237,9 @@ def make_tierlist(with_year_tag: bool = False, with_year_first_visited_tag: bool
         tier_img = make_one_complete_tier(tier, with_year_tag=with_year_tag, with_year_first_visited_tag=with_year_first_visited_tag)
         tierlist.paste(tier_img, (DEFAULT_GAP, y_offset))
         y_offset += tier_img.size[1] + DEFAULT_GAP
+    if DEBUG:
+        end = time.time()
+        print(f"Time taken for make_tierlist({with_year_tag}, {with_year_first_visited_tag}): {end - start} seconds")
     return tierlist
 
 
@@ -207,3 +251,13 @@ if __name__ == "__main__":
     tierlist_with_year_tag.save(TIERLIST_IMAGE_NAME_WITH_YEAR_TAG)
     tierlist_with_year_first_visited_tag = make_tierlist(with_year_first_visited_tag=True)
     tierlist_with_year_first_visited_tag.save(TIERLIST_IMAGE_NAME_WITH_YEAR_FIRST_VISITED_TAG)
+    if DEBUG:
+        import cProfile
+        import pstats
+        cProfile.run("make_tierlist()", "profile")
+        p = pstats.Stats("profile")
+        # p.strip_dirs().sort_stats("time").print_stats()
+        # p.strip_dirs().sort_stats("calls").print_stats()
+        # p.strip_dirs().sort_stats("ncalls").print_stats()
+        p.strip_dirs().sort_stats("cumtime").print_stats()
+        os.remove("profile")
